251936bfb626bcb68713d7d75147fe30cfe85483
Andre Santos <andrefelipe_santos@outlook.com>
2022-02-11 17:54:50 -0300

    Build: função renderScreen

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const keyAPI = '2v2aU6pUEYfXap7E6vig9pUqbHGlw3K5F4iXRdv9';
 const url = `https://api.nasa.gov/planetary/apod?api_key=${keyAPI}&date=2017-07-08`
 const responseSection = document.querySelector('.returned-response');
+const btn = document.querySelector('#button');
+const input =  document.querySelector('#input');
 
 const getJSONFromUrlAPI = async () => {
   const response = await fetch(url);
@@ -9,21 +11,41 @@ const getJSONFromUrlAPI = async () => {
   return data;
 };
 
-const putImageOnScreen = (imageUrl) => {
-  const img = document.createElement('img');
-  img.src = imageUrl;
-  responseSection.appendChild(img);
-};
+// const putImageOnScreen = (imageUrl) => {
+//   const img = document.createElement('img');
+//   img.src = imageUrl;
+//   responseSection.appendChild(img);
+// };
 
-const putExplanationOnScreen = (explanationUrl) => {
+// const putExplanationOnScreen = (explanationUrl) => {
+//   const p = document.createElement('p');
+//   p.innerHTML = explanationUrl;
+//   responseSection.appendChild(p);
+// };
+
+function createElementOfScreen({ url, explanation }) {
+  const div = document.createElement('div');
+  div.className = '';
+  const img = document.createElement('img');
+  img.className = '';
+  img.src = url;
   const p = document.createElement('p');
-  p.innerHTML = explanationUrl;
-  responseSection.appendChild(p);
-};
+  p.className = '';
+  p.innerText = explanation;
+  div.appendChild(img);
+  div.appendChild(p);
+  return div;
+}
+
+async function renderScreen() {
+  const response = await getJSONFromUrlAPI(input);
+  responseSection.appendChild(createElementOfScreen(response));
+}
 
 window.onload = async () => {
-  const response = await getJSONFromUrlAPI();
-  putImageOnScreen(response.url);
-  putExplanationOnScreen(response.explanation);
-  console.log(response)
+  // const response = await getJSONFromUrlAPI();
+  // putImageOnScreen(response.url);
+  // putExplanationOnScreen(response.explanation);
+  // console.log(response)
+  btn.addEventListener('click', renderScreen)
 }
