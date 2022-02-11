61695f6a58ef6149d0331940375fb44df11db19b
André Luis Oliveira Rocha de Moraes <andre.moraes.98@gmail.com>
2022-02-11 15:15:59 -0300

    Adiçao da explicaçao na tela

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const keyAPI = '2v2aU6pUEYfXap7E6vig9pUqbHGlw3K5F4iXRdv9';
 const url = `https://api.nasa.gov/planetary/apod?api_key=${keyAPI}&date=2017-07-08`
-const imgSection = document.querySelector('.returned-image');
+const responseSection = document.querySelector('.returned-response');
 
 const getJSONFromUrlAPI = async () => {
   const response = await fetch(url);
@@ -12,10 +12,18 @@ const getJSONFromUrlAPI = async () => {
 const putImageOnScreen = (imageUrl) => {
   const img = document.createElement('img');
   img.src = imageUrl;
-  imgSection.appendChild(img);
+  responseSection.appendChild(img);
+};
+
+const putExplanationOnScreen = (explanationUrl) => {
+  const p = document.createElement('p');
+  p.innerHTML = explanationUrl;
+  responseSection.appendChild(p);
 };
 
 window.onload = async () => {
   const response = await getJSONFromUrlAPI();
   putImageOnScreen(response.url);
+  putExplanationOnScreen(response.explanation);
+  console.log(response)
 }
