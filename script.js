4845a28410eab7fa29a5f457c242d9c0eda39866
André Luis Oliveira Rocha de Moraes <andre.moraes.98@gmail.com>
2022-02-14 09:40:18 -0300

    Implementaçao da restriçao do ano

--- script.js
+++ script.js
@@ -34,15 +34,22 @@ function createElementOfScreen({ url, explanation, media_type }) {
 
 async function renderScreen() {
   const response = await getJSONFromUrlAPI(input.value);
-  responseSection.innerHTML = '';
-  responseSection.appendChild(createElementOfScreen(response.map((star) => {
-    const objectOfStar = {
-      url: star.url,
-      explanation: star.explanation,
-      media_type: star.media_type,
-    };  
-    return objectOfStar
-  })[0]));
+  const year = parseInt(input.value.split('-')[0]);
+  
+  if (year < 1995) {
+    window.alert('Ano inválido!');
+    input.value = '';
+  } else { 
+    responseSection.innerHTML = '';
+    responseSection.appendChild(createElementOfScreen(response.map((star) => {
+      const objectOfStar = {
+        url: star.url,
+        explanation: star.explanation,
+        media_type: star.media_type,
+      };  
+      return objectOfStar
+    })[0]));
+  }
 }
 
 window.onload = async () => {
